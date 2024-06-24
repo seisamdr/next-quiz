e251f66a0d2e23318259c2f86b4ecd1a1d5b19fe
Seiryo Pramanda <seiryobayu@gmail.com>
2024-06-24 21:01:15 +0700

    fix: fix bug login

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import "server-only";
 
 declare global {
-  // eslint-disable-next-line no-var
-  var cachePrisma: PrismaClient;
+  // eslint-disable-next-line no-var, no-unused-vars
+  var cachedPrisma: PrismaClient;
 }
 
 export let prisma: PrismaClient;
-
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.cachePrisma) {
-    global.cachePrisma = new PrismaClient();
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
   }
-  prisma = global.cachePrisma;
+  prisma = global.cachedPrisma;
 }
