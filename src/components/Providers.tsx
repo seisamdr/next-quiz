37505706cec0100759fae770d6bfc882923bc7e6
Seiryo Pramanda <seiryobayu@gmail.com>
2024-06-24 17:41:27 +0700

    feat: create navbar

---
+++ src/components/Providers.tsx
@@ -0,0 +1,9 @@
+import { SessionProvider } from "next-auth/react";
+
+type Props = { children: React.ReactNode };
+
+const Providers = ({ children }: Props) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default Providers;
